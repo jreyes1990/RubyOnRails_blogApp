b2ce8dcfcbb4d7cc8e1e32555cb856dd74572588
jreyes <jarl1990mt@gmail.com>
2022-07-15 14:56:20 -0600

    Se genero lo siguiente(s) al crear el controlador Articulo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'article/new'
+  get 'article/show'
   get 'welcome', to: "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

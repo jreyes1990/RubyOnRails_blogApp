bd0cb921776f3db74462ed88d7df38bf31fb0b6d
jreyes <jarl1990mt@gmail.com>
2022-07-15 19:42:01 -0600

    Se mejori la ruta de Articulo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
-  get 'articles/show'
   get 'welcome', to: "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end

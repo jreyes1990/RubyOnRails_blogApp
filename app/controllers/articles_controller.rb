3305e4261e76e1e1ade83d8230432bae0a7e3b14
jreyes <jarl1990mt@gmail.com>
2022-07-15 19:39:59 -0600

    Actualizacion metodos de Articulo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,29 @@
 class ArticlesController < ApplicationController
   def index
+    @articulos = Article.all.order('id DESC')
   end
 
   def new
+    @articulo = Article.new
+  end
+
+  def create
+    @articulo = Article.new(article_params)
+    if @articulo.save
+      redirect_to article_path(@articulo), notice: 'El articulo se creo de forma exitosa.'
+    else
+      render :new, status: :unprocessable_entity
+      puts "************************************************"
+      puts @articulo.errors.full_messages
+      puts "************************************************"
+    end
   end
 
   def show
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end

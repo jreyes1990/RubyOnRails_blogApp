7cade42baebd1666f2c2f267d2d7aba1000203c3
jreyes <jarl1990mt@gmail.com>
2022-07-15 23:12:49 -0600

    Configurando controlador para mostrar los articulos y ser la vista principal

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @saludo = "Hola mundo de programadores. Este es un buen comienzo para programar en RAILS"
+    @articulos = Article.all.order('id DESC')
   end
 end

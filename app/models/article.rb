6e0ae64c04a59eb2c0a4e69109328c156c8dcbbe
jreyes <jarl1990mt@gmail.com>
2022-07-15 19:40:20 -0600

    Se actualizo las validaciones de Articulo

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: {message: 'El Titulo no puede estar vacio'}
   validates :body, presence: {message: 'El Cuerpo del titulo no puede estar vacio'}
-  validates :title, length: {maximum: 25, minimum: 5, message: 'Debe ingresar minimo 5 caracteres y maximo 25'}
+  validates :title, length: {maximum: 50, minimum: 5, message: 'Debe ingresar minimo 5 caracteres y maximo 50'}
   validates :body, length: {maximum: 200, minimum: 5, message: 'Debe ingresar minimo 5 caracteres y maximo 200'}
-  validates :body, :uniqueness => {message: 'El Titulo debe ser unico'}
+  validates :title, :uniqueness => {message: 'El Titulo debe ser unico'}
 end

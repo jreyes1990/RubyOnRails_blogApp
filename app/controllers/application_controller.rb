fc320effae62b4bfffda5af669b1f93f90fdd751
jreyes <jarl1990mt@gmail.com>
2022-07-17 22:57:09 -0600

    Mejora template formulario de Articulo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :cargar_registros
+
+  def cargar_registros
+    @categorias = Category.all.order('id DESC')
+  end
 end

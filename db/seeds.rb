cb06e4d9c1c9053b9cf8e1e0bf2f028e8cbf5b11
jreyes <jarl1990mt@gmail.com>
2022-07-20 00:03:48 -0600

    Insercion por medio de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+Article.create(title: 'Impresora CANON', body: 'Descricòn de la impresora, para uso de reportes', user_id: 1)
+Article.create(title: 'Impresora HP', body: 'Descricòn de la impresora, para uso de reportes', user_id: 2)
+Article.create(title: 'Impresora multifuncional HP', body: 'Descricòn de la impresora, para uso de reportes', user_id: 1)

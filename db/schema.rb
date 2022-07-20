009623786519f8e6f5bc5b1d8fb5dfb03bf1fd94
jreyes <jarl1990mt@gmail.com>
2022-07-19 21:56:18 -0600

    Se crea migracion de Articulo y Categoria haciendo referencia a Usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_053539) do
+ActiveRecord::Schema.define(version: 2022_07_20_034447) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -24,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_07_19_053539) do
     t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +53,6 @@ ActiveRecord::Schema.define(version: 2022_07_19_053539) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
+  add_foreign_key "categories", "users"
 end

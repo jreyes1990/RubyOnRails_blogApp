38e1f6bf9d6f650093412c5836667243769f276b
jreyes <jarl1990mt@gmail.com>
2022-07-19 22:56:10 -0600

    Relaciòn de Usuario a Categoria

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+  # Usuario pertenece a Articulo
+  belongs_to :user
+
+  #Validaciones en campos
   validates :name, presence: {message: 'El Nombre no puede estar vacio'}
   validates :color, presence: {message: 'El Color no puede estar vacio'}
   validates :name, length: {maximum: 30, minimum: 5, message: 'Debe ingresar minimo 5 caracteres y maximo 30'}

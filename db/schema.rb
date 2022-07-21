acce0ab622cff260321ddec8ec97ff5ed3ca08d3
jreyes <jarl1990mt@gmail.com>
2022-07-21 00:55:09 -0600

    Se creo el model HasCategory, haciendo referencia a la tabla Articulo y Categorias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_034447) do
+ActiveRecord::Schema.define(version: 2022_07_21_064957) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2022_07_20_034447) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "has_categories", force: :cascade do |t|
+    t.integer "articles_id", null: false
+    t.integer "categories_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["articles_id"], name: "index_has_categories_on_articles_id"
+    t.index ["categories_id"], name: "index_has_categories_on_categories_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +64,6 @@ ActiveRecord::Schema.define(version: 2022_07_20_034447) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "categories", "users"
+  add_foreign_key "has_categories", "articles", column: "articles_id"
+  add_foreign_key "has_categories", "categories", column: "categories_id"
 end

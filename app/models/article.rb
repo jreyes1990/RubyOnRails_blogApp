f16ea042371c9372e6367bb7bf6436d44178be9d
jreyes <jarl1990mt@gmail.com>
2022-07-21 01:11:47 -0600

    Haciendo referencia a la tabla Articulo y Categorias. Relacion tabla(s) Categorias y Articulos a traves de la tabla Has_Categoria

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,9 @@ class Article < ApplicationRecord
   validates :title, length: {in: 5..25, message: 'Debe ingresar minimo 5 caracteres y maximo 25'}
   validates :body, length: {in: 25..200, message: 'Debe ingresar minimo 25 caracteres y maximo 200'}
   validates :title, :uniqueness => {message: 'El Titulo debe ser unico'}
+
+  #Tabla Has_categorias haciendo referencia a la tabla Articulo
+  has_many :has_categories
+  #Relacion a la tabla categories a traves de la tabla has_categories
+  has_many :categories, through: :has_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -7,4 +7,9 @@ class Category < ApplicationRecord
   validates :name, length: {in: 7..20, message: 'Debe ingresar minimo 7 caracteres y maximo 20'}
   validates :color, length: {in: 4..7, message: 'Debe ingresar minimo 4 caracteres y maximo 7'}
   validates :name, :uniqueness => {message: 'El Nombre debe ser unico'}
+
+  #Tabla Has_categorias haciendo referencia a la tabla Categoria
+  has_many :has_categories
+  #Relacion a la tabla articles a traves de la tabla has_categories
+  has_many :articles, through: :has_categories
 end

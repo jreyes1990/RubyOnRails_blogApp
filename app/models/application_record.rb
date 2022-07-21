a44c398327b55a28156b9665db5eb1f41232a935
jreyes <jarl1990mt@gmail.com>
2022-07-21 00:14:51 -0600

    Se agregaron metodos de autenticacion

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  #Metodos de autenticacion
+  protected
+  def authenticate_editor!
+    redirect_to root_path unless user_signed_in? && current_user.is_editor_user?
+  end
+
+  def authenticate_admin!
+    redirect_to root_path unless user_signed_in? && current_user.is_admin_user?
+  end
 end

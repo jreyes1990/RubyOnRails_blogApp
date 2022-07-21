f5af2947091f3f2944d8c3929e6c8bf4d37fa71b
jreyes <jarl1990mt@gmail.com>
2022-07-21 00:14:21 -0600

    Se agregaron metodos de autenticacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   def cargar_registros
     @categorias = Category.all.order('id DESC')
   end
+
+  def authenticate_editor!
+    redirect_to root_path, alert: "No tiene permiso para entrar aqui." unless user_signed_in? && current_user.is_editor_user?
+  end
+
+  def authenticate_admin!
+    redirect_to root_path, alert: "No tienes suficientes permisos para entrar aqui." unless user_signed_in? && current_user.is_admin_user?
+  end
 end

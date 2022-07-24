3cfec544c99464d774d9e0d88a7808e365c64800
jreyes1990 <jarl1990mt@gmail.com>
2022-07-24 17:41:09 -0600

    Se creo una ruta de busqueda Articulos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get "articles/search"
+
   devise_for :users
+
   get 'welcome', to: "welcome#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
   resources :categories

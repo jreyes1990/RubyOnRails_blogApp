49fd8bee25224f4db951eeaf4ccf7b0b10d48343
jreyes1990 <jarl1990mt@gmail.com>
2022-07-24 23:06:33 -0600

    Actualizacion de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+  get 'contacts/create'
   get "articles/search"
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    session: 'users/sessions',
+    passwords: 'users/passwords'
+  }
 
   get 'welcome', to: "welcome#index"
+  get 'contacto', to: "welcome#contacto"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
   resources :categories
+  resources :contacts, only: [:create]
 
   root "articles#index"
 end

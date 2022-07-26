d0634cf0f96796d24a6c473289a919349de16a54
jreyes1990 <jarl1990mt@gmail.com>
2022-07-25 23:28:33 -0600

    Mejora de metodos de Contacto

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,24 @@
 class ContactsController < ApplicationController
+  def new
+    @contacto = Contact.new
+  end
+
   def create
     @contacto = Contact.new(contacts_params)
 
     respond_to do |format|
       if @contacto.save
-        format.html {redirect_to contacts_path(@contacto), notice: "El correo se creo exitosamente."}
+        format.html {redirect_to root_path, notice: "El correo se creo exitosamente."}
+        format.json {render :show, status: :created, location: @contacto}
       else
-        format.html {redirect_to contacts_path(@contacto), alert: "Problema al enviar su corre."}
+        format.html {redirect_to :new, alert: "Problema al enviar su corre."}
+        format.json {render json: @contacto.errors, status: :unprocessable_entity}
       end
     end
   end
 
   private
   def contacts_params
-    params.require(:contact).permit(:title, :email, :description)
+    params.require(:contact).permit(:title, :description, :email)
   end
 end

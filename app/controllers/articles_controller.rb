251e9cea329f1597bc83933aee32ca9981928106
jreyes1990 <jarl1990mt@gmail.com>
2022-07-27 22:18:57 -0600

    Mejoras del proyecto, configuracion para la carga de imagenes utilizando paperclip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -83,6 +83,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :body, :categories)
+    params.require(:article).permit(:title, :body, :categories, :img_art)
   end
 end
